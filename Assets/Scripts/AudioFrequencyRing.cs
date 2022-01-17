65f55e74de97c3eac3dac8d1c9aa4bd53b44bb15
Collin <17454325+Collin001998@users.noreply.github.com>
2022-01-17 17:37:31 +0100

    fixes
    - Subtitles

--- Assets/Scripts/AudioFrequencyRing.cs
+++ Assets/Scripts/AudioFrequencyRing.cs
@@ -8,9 +8,9 @@ public class AudioFrequencyRing : MonoBehaviour
     public Runningtap.AnalyzeAudio audioData;
 
     public GameObject sampleCubePrefab;
-    public Vector3 StartScale = new Vector3(1f,1f,1f);
+    public Vector3 StartScale = new Vector3(-2f,-2f,-2f);
     public float Radius = 20f;
-    public float Sensitivity = 2f;
+    public float Sensitivity = 250f;
 
     private GameObject[] sampleCube;
 
@@ -30,8 +30,9 @@ public class AudioFrequencyRing : MonoBehaviour
             instance.transform.eulerAngles = new Vector3(0, 0, 0);
             sampleCube[i] = instance;
         }
+        
         this.transform.rotation = Quaternion.Euler(-90, 0, 0);
-        //this.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        this.transform.localScale = new Vector3(0.08f, 0.08f, 0.08f);
     }
 	
 	void Update ()
