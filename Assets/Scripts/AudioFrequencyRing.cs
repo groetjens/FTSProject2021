2e3dcc2307e82c68f67e24f105f5bf062686095f
Collin <17454325+Collin001998@users.noreply.github.com>
2022-01-17 16:04:57 +0100

    Visual audio, change video
    -visual audio on both sides
    -changed video from LOTR to funny girl
    -fixed sync within audio and video

--- Assets/Scripts/AudioFrequencyRing.cs
+++ Assets/Scripts/AudioFrequencyRing.cs
@@ -8,8 +8,8 @@ public class AudioFrequencyRing : MonoBehaviour
     public Runningtap.AnalyzeAudio audioData;
 
     public GameObject sampleCubePrefab;
-    public Vector3 StartScale = new Vector3(1,1,1);
-    public float Radius = 100f;
+    public Vector3 StartScale = new Vector3(1f,1f,1f);
+    public float Radius = 20f;
     public float Sensitivity = 2f;
 
     private GameObject[] sampleCube;
@@ -27,10 +27,12 @@ public class AudioFrequencyRing : MonoBehaviour
             instance.name = "SampleCube_" + i;
             transform.eulerAngles = new Vector3(0, -angle * i, 0);
             instance.transform.position = Vector3.forward * Radius;
-            instance.transform.eulerAngles = new Vector3(90, 0, 0);
+            instance.transform.eulerAngles = new Vector3(0, 0, 0);
             sampleCube[i] = instance;
         }
-	}
+        this.transform.rotation = Quaternion.Euler(-90, 0, 0);
+        //this.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+    }
 	
 	void Update ()
     {
---
+++ Assets/SyncContent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyncContent : MonoBehaviour
+{
+    public UnityEngine.Video.VideoPlayer VideoPlayer;
+    public UnityEngine.AudioSource AudioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        VideoPlayer.Prepare();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (VideoPlayer.isPlaying)
+        {
+
+        } else if (VideoPlayer.isPrepared)
+        {
+            VideoPlayer.Play();
+            AudioSource.Play();
+            StartCoroutine(FindObjectOfType<SubtitleDisplayer>().Begin());
+        }
+    }
+}
